ef217588dcf6cad7aec5dde447adbf6b29109c25
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-09-18 12:53:06 -0400

    Update linux_dependencies.bash

--- install/linux_dependencies.bash
+++ install/linux_dependencies.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # nodejs.
 sudo apt-get purge nodejs npm
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get install -y nodejs
 # bower & grunt-cli global install.
 sudo npm install -g bower grunt-cli
