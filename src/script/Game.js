315fbbb427418b6079b44c499a24fa443574d031
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-09-17 21:12:29 -0400

    Movement example

--- src/script/Game.js
+++ src/script/Game.js
@@ -1,61 +1,64 @@
 ENGINE.Game = {
 
+  posX : 0,
+  posY : 0,
+  currentMove : null,
+  movement : {
+      "w" : {
+          "x" : 0,
+          "y" : -1
+      },
+      "s" : {
+          "x" : 0,
+          "y" : 1
+      },
+      "a" : {
+          "x" : -1,
+          "y" : 0 
+      },
+      "d" : {
+          "x" : 1,
+          "y" : 0
+      }
+  },
+
   create: function() {
 
   },
 
   step: function(dt) {
-
-    /* update your game logic here */
-
+    var move = this.currentMove;
+    if ( move !== null ) {
+      var speedScale = 4;
+      this.posX += speedScale * move.x;
+      this.posY += speedScale * move.y;
+    }
   },
 
   render: function() {
-
-    /* get reference to the application */
-
-    var app = this.app;
-
-    /* get reference to drawing surface */
-
-    var layer = this.app.layer;
-
-    /* clear screen */
-
-    layer.clear("#222");
-
-    /* save all setting of drawing pointer */
-    
-    layer.save();
-
-    /* translate drawing pointer to the center of screen */
-
-    layer.translate(app.center.x, app.center.y - 30);
-
-    /* set rotation point of all sprites/images to their center */
-
-    layer.align(0.5, 0.5);
-
-    /* tell the drawing pointer to scale everything x 3 */
-
-    layer.scale(3, 3);
-
-    /* draw sprite */
-
-    layer.drawImage(app.images.car_black_1, 0, 0);
-
-    /* draw text - this is not affected by align */
-
-    layer
+    var app = this.app; // get reference to the application
+    var layer = this.app.layer; // get reference to drawing surface
+    layer.clear("#222"); //clear screen 
+    layer.save(); // save all setting of drawing pointer
+    layer.translate(app.center.x + this.posX, app.center.y + this.posY); //translate drawing pointer to the center of screen 
+    layer.align(0.5, 0.5); //set rotation point of all sprites/images to their center 
+    layer.scale(3, 3); //tell the drawing pointer to scale everything x 3 
+    layer.drawImage(app.images.car_black_1, 0, 0); //draw sprite 
+    layer //draw text - this is not affected by align 
       .fillStyle("#fff")
       .textAlign("center")
       .fillText("Do you remember me?", 0, 84)
       .fillText("Find me in script/Game.js", 0, 108);
+    layer.restore(); //restore drawing pointer to its previous state 
+  },
 
-    /* restore drawing pointer to its previous state */
-
-    layer.restore();
-
+  keydown: function(event) {
+    if ( event.key in this.movement ){
+        this.currentMove =  this.movement[ event.key ];
+    }
+  },
+  
+  keyup: function(event) {
+      this.currentMove = null;
   }
-
 };
--- src/script/Main.js
+++ src/script/Main.js
@@ -3,19 +3,18 @@ var app = new PLAYGROUND.Application({
   smoothing: false,
 
   create: function() {
-
-    /* things to preload */
-
-    this.loadImage("car_black_1");
-
+    // things to preload
+    var colors = ["black","blue","green","red","yellow"];
+    for (i = 0; i < colors.length; ++i ) {
+        for (j = 1; j < 6; ++j ) {
+            this.loadImage("car_"+colors[i]+"_"+j);
+        }
+    }
   },
 
   ready: function() {
-
     /* after preloading route events to the game state */
-
     this.setState(ENGINE.Game);
-
   }
 
 });
